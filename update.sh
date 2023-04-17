15b8392ad6a7273f202cb66b674940d53a7c901e
PencilMario <Sir.P@foxmail.com>
2023-04-17 19:13:12 +0800

    换用循环(

--- update.sh
+++ update.sh
@@ -6,30 +6,21 @@ git reset --hard
 git pull;
 
 
-# Check the first destination directory and copy the files if it exists
-# 
-if [ -d "/home/steam/Steam/steamapps/common/l4d2versus/left4dead2" ]; then
-  \cp -r /home/steam/L4D2-Competitive-Rework/addons/sourcemod/configs/* /home/steam/Steam/steamapps/common/l4d2versus/left4dead2/addons/sourcemod/configs/;
-  \cp -r /home/steam/L4D2-Competitive-Rework/addons/sourcemod/data/* /home/steam/Steam/steamapps/common/l4d2versus/left4dead2/addons/sourcemod/data/;
-  \cp -r /home/steam/L4D2-Competitive-Rework/addons/sourcemod/gamedata/* /home/steam/Steam/steamapps/common/l4d2versus/left4dead2/addons/sourcemod/gamedata/;
-  \cp -r /home/steam/L4D2-Competitive-Rework/addons/sourcemod/plugins/* /home/steam/Steam/steamapps/common/l4d2versus/left4dead2/addons/sourcemod/plugins/;
-  \cp -r /home/steam/L4D2-Competitive-Rework/addons/sourcemod/translations/* /home/steam/Steam/steamapps/common/l4d2versus/left4dead2/addons/sourcemod/translations/;
+directories=("/home/steam/Steam/steamapps/common/l4d2versus/left4dead2" "/home/steam/Steam/steamapps/common/l4d2/left4dead2")
 
-  \cp -r /home/steam/L4D2-Competitive-Rework/addons/stripper/* /home/steam/Steam/steamapps/common/l4d2versus/left4dead2/addons/stripper/;
-  \cp -r /home/steam/L4D2-Competitive-Rework/scripts/vscripts/* /home/steam/Steam/steamapps/common/l4d2versus/left4dead2/scripts/vscripts/;
-  \cp -r /home/steam/L4D2-Competitive-Rework/cfg/* /home/steam/Steam/steamapps/common/l4d2versus/left4dead2//cfg/;
-fi
-# Check the second destination directory and copy the files if it exists
-if [ -d "/home/steam/Steam/steamapps/common/l4d2/left4dead2" ]; then
-  \cp -r /home/steam/L4D2-Competitive-Rework/addons/sourcemod/configs/* /home/steam/Steam/steamapps/common/l4d2/left4dead2/addons/sourcemod/configs/;
-  \cp -r /home/steam/L4D2-Competitive-Rework/addons/sourcemod/data/* /home/steam/Steam/steamapps/common/l4d2/left4dead2/addons/sourcemod/data/;
-  \cp -r /home/steam/L4D2-Competitive-Rework/addons/sourcemod/gamedata/* /home/steam/Steam/steamapps/common/l4d2/left4dead2/addons/sourcemod/gamedata/;
-  \cp -r /home/steam/L4D2-Competitive-Rework/addons/sourcemod/plugins/* /home/steam/Steam/steamapps/common/l4d2/left4dead2/addons/sourcemod/plugins/;
-  \cp -r /home/steam/L4D2-Competitive-Rework/addons/sourcemod/translations/* /home/steam/Steam/steamapps/common/l4d2/left4dead2/addons/sourcemod/translations/;
-
-  \cp -r /home/steam/L4D2-Competitive-Rework/addons/stripper/* /home/steam/Steam/steamapps/common/l4d2/left4dead2/addons/stripper/;
-  \cp -r /home/steam/L4D2-Competitive-Rework/scripts/vscripts/* /home/steam/Steam/steamapps/common/l4d2/left4dead2/scripts/vscripts/;
-  \cp -r /home/steam/L4D2-Competitive-Rework/cfg/* /home/steam/Steam/steamapps/common/l4d2/left4dead2//cfg/;
-fi
+for dir in "${directories[@]}"; do
+    if [ -d "$dir" ]; then
+        \cp -r /home/steam/L4D2-Competitive-Rework/addons/sourcemod/configs/* "$dir/addons/sourcemod/configs/";
+        \cp -r /home/steam/L4D2-Competitive-Rework/addons/sourcemod/data/* "$dir/addons/sourcemod/data/";
+        \cp -r /home/steam/L4D2-Competitive-Rework/addons/sourcemod/gamedata/* "$dir/addons/sourcemod/gamedata/";
+        \cp -r /home/steam/L4D2-Competitive-Rework/addons/sourcemod/plugins/* "$dir/addons/sourcemod/plugins/";
+        \cp -r /home/steam/L4D2-Competitive-Rework/addons/sourcemod/translations/* "$dir/addons/sourcemod/translations/";
+        \cp -r /home/steam/L4D2-Competitive-Rework/scripts/* "$dir/scripts/";
+        \cp -r /home/steam/L4D2-Competitive-Rework/cfg/* "$dir/cfg/";
+        echo "Updated | $dir"
+    else
+        echo "Unexist | $dir "
+    fi
+done
 
 echo "File Copy Success";
--- update_full.sh
+++ update_full.sh
@@ -6,14 +6,15 @@ git reset --hard
 git pull;
 
 
-# Check the first destination directory and copy the files if it exists
-# 
-if [ -d "/home/steam/Steam/steamapps/common/l4d2versus/left4dead2" ]; then
-  \cp -r /home/steam/L4D2-Competitive-Rework/* /home/steam/Steam/steamapps/common/l4d2versus/left4dead2/;
-fi
-# Check the second destination directory and copy the files if it exists
-if [ -d "/home/steam/Steam/steamapps/common/l4d2/left4dead2" ]; then
-  \cp -r /home/steam/L4D2-Competitive-Rework/* /home/steam/Steam/steamapps/common/l4d2/left4dead2/;
-fi
+directories=("/home/steam/Steam/steamapps/common/l4d2versus/left4dead2" "/home/steam/Steam/steamapps/common/l4d2/left4dead2")
+
+for dir in "${directories[@]}"; do
+    if [ -d "$dir" ]; then
+        \cp -r /home/steam/L4D2-Competitive-Rework/* "$dir/";
+        echo "Updated | $dir"
+    else
+        echo "Unexist | $dir "
+    fi
+done
 
 echo "File Copy Success";
