bbe143443d4d3ea690b4bd3ece11087f9fccd0f0
PencilMario <Sir.P@foxmail.com>
2023-04-17 17:49:23 +0800

    更新测试

--- update.sh
+++ update.sh
@@ -6,9 +6,15 @@ git pull;
 
 # Check the first destination directory and copy the files if it exists
 if [ -d "/home/steam/Steam/steamapps/common/l4d2versus/left4dead2" ]; then
-  \cp -r /home/steam/L4D2-Competitive-Rework/* /home/steam/Steam/steamapps/common/l4d2versus/left4dead2
+  \cp -r /home/steam/L4D2-Competitive-Rework/addons/sourcemod/* /home/steam/Steam/steamapps/common/l4d2versus/left4dead2/addons/sourcemod/;
+  \cp -r /home/steam/L4D2-Competitive-Rework/addons/stripper/* /home/steam/Steam/steamapps/common/l4d2versus/left4dead2/addons/stripper/;
+  \cp -r /home/steam/L4D2-Competitive-Rework/scripts/vscripts/* /home/steam/Steam/steamapps/common/l4d2versus/left4dead2/scripts/vscripts/;
+  \cp -r /home/steam/L4D2-Competitive-Rework/cfg/* /home/steam/Steam/steamapps/common/l4d2versus/left4dead2//cfg/;
 fi
 # Check the second destination directory and copy the files if it exists
 if [ -d "/home/steam/Steam/steamapps/common/l4d2/left4dead2" ]; then
-  \cp -r /home/steam/L4D2-Competitive-Rework/* /home/steam/Steam/steamapps/common/l4d2/left4dead2
+  \cp -r /home/steam/L4D2-Competitive-Rework/addons/sourcemod/* /home/steam/Steam/steamapps/common/l4d2/left4dead2/addons/sourcemod/;
+  \cp -r /home/steam/L4D2-Competitive-Rework/addons/stripper/* /home/steam/Steam/steamapps/common/l4d2/left4dead2/addons/stripper/;
+  \cp -r /home/steam/L4D2-Competitive-Rework/scripts/vscripts/* /home/steam/Steam/steamapps/common/l4d2/left4dead2/scripts/vscripts/;
+  \cp -r /home/steam/L4D2-Competitive-Rework/cfg/* /home/steam/Steam/steamapps/common/l4d2/left4dead2//cfg/;
 fi
