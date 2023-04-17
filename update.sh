2262fc6a51d63227b7f2e04c69cd5acc6cce716a
PencilMario <Sir.P@foxmail.com>
2023-04-17 12:02:44 +0800

    update again

--- update.sh
+++ update.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+cd /home/steam/L4D2-Competitive-Rework/;
+git pull;
+
+
 # Check the first destination directory and copy the files if it exists
 if [ -d "/home/steam/Steam/steamapps/common/l4d2versus/left4dead2" ]; then
   \cp -r /home/steam/L4D2-Competitive-Rework/* /home/steam/Steam/steamapps/common/l4d2versus/left4dead2
