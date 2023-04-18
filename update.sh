18b2faf261a16f662e016a007b41806fb0d43f3c
PencilMario <Sir.P@foxmail.com>
2023-04-18 18:50:08 +0800

    清理垃圾

--- update.sh
+++ update.sh
@@ -10,6 +10,22 @@ directories=("/home/steam/Steam/steamapps/common/l4d2versus/left4dead2" "/home/s
 
 for dir in "${directories[@]}"; do
     if [ -d "$dir" ]; then
+
+        # 可以热更新的内容
+        # 其他文件残余内容短时间内应该不会影响 直接覆盖即可
+
+        #rm -rf "$dir/addons/sourcemod/configs/"*
+        #rm -rf "$dir/addons/sourcemod/data/"*
+        #rm -rf "$dir/addons/sourcemod/gamedata/"*
+        rm -rf "$dir/addons/sourcemod/plugins/"*
+        #rm -rf "$dir/addons/sourcemod/translations/"*
+        #rm -rf "$dir/scripts/vscripts/"*
+
+        #rm -rf "$dir/cfg/cfgogl/"*
+        #rm -rf "$dir/cfg/mixmap/"*
+        #rm -rf "$dir/cfg/sourcemod/"*
+        #rm -rf "$dir/cfg/stripper/"*
+
         \cp -r /home/steam/L4D2-Competitive-Rework/addons/sourcemod/configs/* "$dir/addons/sourcemod/configs/";
         \cp -r /home/steam/L4D2-Competitive-Rework/addons/sourcemod/data/* "$dir/addons/sourcemod/data/";
         \cp -r /home/steam/L4D2-Competitive-Rework/addons/sourcemod/gamedata/* "$dir/addons/sourcemod/gamedata/";
--- update_full.sh
+++ update_full.sh
@@ -10,7 +10,26 @@ directories=("/home/steam/Steam/steamapps/common/l4d2versus/left4dead2" "/home/s
 
 for dir in "${directories[@]}"; do
     if [ -d "$dir" ]; then
+        rm -rf "$dir/addons/sourcemod/"*
+        rm -rf "$dir/addons/metamod/"*
+        rm -rf "$dir/addons/l4dtoolz/"*
+        rm -rf "$dir/addons/stripper/"*
+        rm -rf "$dir/scripts/vscripts/"*
+        rm -rf "$dir/models/player/custom_player/"*
+        rm -rf "$dir/sound/kodua/fortnite_emotes/"*
+        # 剩下三个cfg应该不会被删除
+        rm -rf "$dir/cfg/cfgogl/"*
+        rm -rf "$dir/cfg/mixmap/"*
+        rm -rf "$dir/cfg/sourcemod/"*
+        rm -rf "$dir/cfg/stripper/"*
+
+        rm -f "$dir/l4dtoolz.dll"
+        rm -f "$dir/l4dtoolz.so"
+        rm -f "$dir/l4dtoolz.vdf"
+        rm -f "$dir/metamod.vdf"
+
         \cp -r /home/steam/L4D2-Competitive-Rework/* "$dir/";
+
         echo "Updated | $dir"
     else
         echo "Unexist | $dir "
