e1db125d99fb5d4cc9245bb97b877ed178da1ca9
PencilMario <Sir.P@foxmail.com>
2023-04-19 09:30:28 +0800

    保留database.cfg

--- update_full.sh
+++ update_full.sh
@@ -10,7 +10,7 @@ directories=("/home/steam/Steam/steamapps/common/l4d2versus/left4dead2" "/home/s
 
 for dir in "${directories[@]}"; do
     if [ -d "$dir" ]; then
-        find "$dir/addons/sourcemod/" ! -path "$dir/addons/sourcemod/logs*" -delete
+        find "$dir/addons/sourcemod/" ! -path "$dir/addons/sourcemod/logs*" ! -path "$dir/addons/sourcemod/data/database.cfg" -delete
         rm -rf "$dir/addons/metamod/"*
         rm -rf "$dir/addons/l4dtoolz/"*
         rm -rf "$dir/addons/stripper/"*
